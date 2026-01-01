be4646072fa514c708b2a1960a614d38d3971e7b
lucasstrazzeri-afk <lucas.strazzeri@gmail.com>
2026-01-01 14:24:40 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'alpe-v1';
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html'])));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
+});
